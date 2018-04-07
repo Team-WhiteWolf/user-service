a3877a1661f7f99bf738a9f1a7372f855f17c27e
Tobias Wältken <tobias.w@ltken.de>
2018-04-07 23:58:32 +0200

    Add a sample mysql connection

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
 const azure = require('azure');
-const key = 'Endpoint=sb://servicequeues.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=AUNiefT6dHz3ivqbYvpteI+LlwvOWE2M0OleRycSXzs=';
-const queueSvc = azure.createServiceBusService(key);
+const mysql = require('mysql2');
+
+const azureKey = 'Endpoint=sb://servicequeues.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=AUNiefT6dHz3ivqbYvpteI+LlwvOWE2M0OleRycSXzs=';
+const sqlAcc = {
+    host: 'ww-user-db.mysql.database.azure.com',
+    user: 'mysqldbuser@ww-user-db',
+    password: 'z5CNFHhY!',
+    port: 3306,
+    ssl: true
+};
+
+const queueSvc = azure.createServiceBusService(azureKey);
+const sqlConnection = mysql.createConnection(sqlAcc);
 
 var message = {
 	keks: 'test',
-    body: 'Test message',
-    customProperties: {
-        testproperty: 'TestValue'
+	body: 'Test message',
+	customProperties: {
+		testproperty: 'TestValue'
 	}
 };
 
 queueSvc.sendQueueMessage('user-send', message, function(error){
-    if(!error){
+	if(!error){
 		console.log('Message sent!');
-    }
+	}
 });
 
 queueSvc.receiveQueueMessage('user-send', function(error, receivedMessage){
-    if(!error){
-        // Message received and deleted
-        console.log(receivedMessage.body);
-    }
+	if(!error){
+		// Message received and deleted
+		console.log(receivedMessage.body);
+	}
+});
+
+sqlConnection.connect(function(err) {
+	if (err) throw err;
+	console.log("Connected!");
 });
