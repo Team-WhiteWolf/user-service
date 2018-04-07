f6665f72aeb7d53a68c23cbaf6e19a3955feff7c
Tobias Wältken <tobias.w@ltken.de>
2018-04-07 21:34:57 +0200

    Change the hello world message

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
+    res.end('Keksdose');
 }).listen(process.env.PORT || 8080);
