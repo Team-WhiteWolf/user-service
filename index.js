b9d062114d2f44c94ce3ff598161ce0d196973a6
Tobias Wältken <tobias.w@ltken.de>
2018-04-07 22:33:50 +0200

    Add shared access key

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var azure = require('azure-storage');
-var queueSvc = azure.createQueueService();
+var queueSvc = azure.createQueueService("Endpoint=sb://servicequeues.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=AUNiefT6dHz3ivqbYvpteI+LlwvOWE2M0OleRycSXzs=");
 
 queueSvc.createMessage('user-send', "Hello world!", function(error, results, response){
 	if(!error){
